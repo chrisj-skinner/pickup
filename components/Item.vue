427a865ad18e677584cd38b06b428f3e27ba3c12
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-04 19:10:37 +0100

    :tada: Init Commit

---
+++ components/Item.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <div>
+      <figure class="image is-square">
+        <img src="https://bulma.io/images/placeholders/128x128.png" />
+      </figure>
+      <h4 class="has-text-left">{{ item.title }}</h4>
+      <p class="has-text-left is-size-7">{{ parsedDate }} @{{ item.time }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    // eslint-disable-next-line
+    item: Object
+  },
+  computed: {
+    parsedDate() {
+      const itemDate = new Date(this.item.date)
+      return itemDate.toDateString()
+    }
+  }
+}
+</script>
+
+<style scoped></style>
