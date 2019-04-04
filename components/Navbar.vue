2c42cb2532cfce2550bd6f472b8695b6f782745d
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-04 14:17:01 +0100

    :tada: Init commit

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="links">
+    <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+      >Documentation</a
+    >
+    <a
+      href="https://github.com/nuxt/nuxt.js"
+      target="_blank"
+      class="button--grey"
+      >GitHub</a
+    >
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.links {
+  padding-top: 15px;
+}
+</style>
