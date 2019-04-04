78745203de875592e6e2ea1f4426eae58a62be69
Chris Skinner <c.skinner@blueyonder.co.uk>
2019-04-04 15:12:19 +0100

    :tada: Init Commit

---
+++ pages/item/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Item #{{ this.$route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      titleTemplate: 'Item #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'All there is to know about Item #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped></style>
